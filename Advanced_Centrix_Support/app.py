d23004fc83067858e9f22bd987bd3e6b541559e4
Priyanshu Shukla <priyanshushukla265@gmail.com>
2026-02-11 10:58:35 +0530

    Update app.py

--- Advanced_Centrix_Support/app.py
+++ Advanced_Centrix_Support/app.py
@@ -1325,4 +1325,5 @@ def health():
 
 if __name__ == '__main__':
     print("CentrixSupport - Mental Health AI Chatbot")
-    app.run(host="0.0.0.0", port=8000, debug=True, use_reloader=False)
+
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
